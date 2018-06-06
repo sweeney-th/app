ed9c1d9804176de4a35e1db0353a2cd40f0eee9c
thadryan <thadryan@slater.slater>
2018-06-06 14:32:27 -0400

    Database is up and running

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# import and register the Thing class
+
+
+### Thing class and related
+
+from collection.models import Thing
+
+# create an admin class for it
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    # these are attributes of the Thing
+    list_display = ('name', 'description',)
+    # auto slug
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Thing, ThingAdmin)
--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Thing(models.Model):
+    # it's name is a character field up to 255
+    name = models.CharField(max_length = 255)
+    # a text field describes it
+    description = models.TextField()
+    slug = models.SlugField(unique = True)
